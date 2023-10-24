6cce64c3d21d3d906ec63aad9f8edd76d062bc42
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2023-10-24 21:09:25 +0100

    add logo

--- assets/js/fashion.js
+++ assets/js/fashion.js
@@ -30,8 +30,38 @@ document.addEventListener('DOMContentLoaded', function(){
         answer[index].classList.toggle("hidden");
       });
     });
-
-
+//validation formulaire
+document.getElementById('my-form').addEventListener('submit', function(event) {
+  var prenom = document.getElementById('prenom').value;
+  var nom = document.getElementById('nom').value;
+  var email = document.getElementById('email').value;
+  var telephone = document.getElementById('telephone').value;
+  var motdepasse = document.getElementById('motdepasse').value;
+  
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var telPattern = /^\d{10}$/;
+  var passPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  
+  if (prenom.trim() === '' || nom.trim() === '' || email.trim() === '' || telephone.trim() === '' || motdepasse.trim() === '') {
+    alert('Tous les champs sont obligatoires. Veuillez remplir tous les champs.');
+    event.preventDefault();
+  } else {
+    if (!emailPattern.test(email)) {
+        alert('Adresse électronique invalide');
+        event.preventDefault();
+    }
+    
+    if (!telPattern.test(telephone)) {
+        alert('Téléphone portable invalide');
+        event.preventDefault();
+    }
+    
+    if (!passPattern.test(motdepasse)) {
+        alert('Mot de passe invalide. Le mot de passe doit contenir au moins 8 caractères, dont au moins une lettre majuscule, une lettre minuscule et un chiffre.');
+        event.preventDefault();
+    }
+}
+});
 
 })
 
